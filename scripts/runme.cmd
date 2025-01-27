d73f575eda0402bce10399ec529494a624c34915
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-01-27 10:41:09 -0800

    Add temporary helper to aid tests

---
+++ scripts/runme.cmd
@@ -0,0 +1,1 @@
+"./bonsai/bonsai.exe" "./src/Pirouette.bonsai" -p TaskLogicPath="..\local\AindBehaviorPirouetteTaskLogic.json" -p RigPath="..\local\AindBehaviorPirouetteRig.json" -p SessionPath="..\local\AindBehaviorSessionModel.json"
